1303f835aef23c63e91c2ad8ef6918c6d767592b
Joshua Lim <jmlim6@icloud.com>
2023-03-19 15:45:11 -0400

    Initial commit

---
+++ How to Start Programming/Part 2/dog_years.py
@@ -0,0 +1,11 @@
+# Take the users age
+age = input('Enter your age: ')
+
+# Convert the users age to an integer
+age = int(age)
+
+# Calculate the age in dog years
+dog_age = age * 7
+
+# Print the age in dog years
+print('Your age in dog years is ' + str(dog_age))
---
+++ How to Start Programming/Part 2/syntax.py
@@ -0,0 +1,43 @@
+# Prints 'Hello, World!' to the console
+print('Hello, World!')
+
+# Example of an Int in Python
+age = 19
+
+# Example of a Float in Python
+height = 6.25 # Translates to 6 foot 3 inches
+
+# Example of a Double in Python
+height = 6.25 # Translates to 6 foot 3 inches
+
+# Example of a String in Python
+name = 'Joshua Lim'
+
+# Example of a Boolean in Python
+is_male = true
+
+# Example of an Integer List in Python
+integer_array = [1, 2, 3, 4, 5]
+
+# Example of a Float List in Python
+float_array = [1.1, 2.2, 3.3, 4.4, 5.5]
+
+# Example of a Double List in Python
+double_array = [1.11, 2.22, 3.33, 4.44, 5.55]
+
+# Example of a String List in Python
+string_array = ["Hello", "World", "Python", "Programming", "Language"]
+
+# Example of a Boolean List in Python
+boolean_array = [True, False, True, False, True]
+
+# Example of a function that takes two integers and returns the sum of them
+def add_two_ints(a, b):
+    c = a + b
+    return c
+
+# Example of calling the function
+x = 5
+y = 6
+z = add_two_ints(x, y)
+print(z) # Prints 11
